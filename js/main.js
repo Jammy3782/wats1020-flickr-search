cb13401c5c4fce2bc8efffda3a6d134174e5f403
Jammy3782 <jammy3782@gmail.com>
2017-03-04 13:46:39 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,16 +6,15 @@
 //
 // Allow users to click the images to see a larger version with more information.
 
-    // Place your code here, inside the document ready handler.
-});
+ // Place your code here, inside the document ready handler.
 $(document).on('ready', function(){
   var searchImages = function(tags) {
       var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    console.log(tags);
+    console.log(tags);}
     $('#images').innerHTML = '<li class="search-throbber">Searching...</li>';  // Flickr reveal  a searchable JSON Feed you can access via jQuery's $.getJSON()
     $.getJSON(flickrAPI, {
       tags: tags,
-      tagmode: "any"
+      tagmode: "any", 
    // image search results
       format: "json"
     }).done(function( data ) {
@@ -23,14 +22,15 @@ $(document).on('ready', function(){
       // empties image container
       $('h1.search-title').first()[0].innerHTML = "Search for: " + tags;
       $.each( data.items, function( i, item ) {
-        var newListItem = $("<li>")
+        var newListItem = $("<li>"); 
      // image information
         var newTitle = $('<p class="image-title">').html(item.title).appendTo(newListItem);
         var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newListItem);
         var newDescription = $('<p class="image-description">').html(item.description).appendTo(newListItem);
         var newLink = $('<a>').attr('href', item.link).text('View on Flickr.').appendTo(newListItem);
 
-   
+ 
   });
-
 });
+});  
+  
