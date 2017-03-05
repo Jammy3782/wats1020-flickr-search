de9819c70f74a7d3bde37dd04fc8b0ab1844adc2
Jammy3782 <jammy3782@gmail.com>
2017-03-04 16:28:57 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 // Asynchronous Flickr Search
-
-
 // method. Use this to allow users to search for a tag or comma-separated list
 // of tags and view the images that are found.
 //
@@ -22,7 +20,7 @@ $(document).on('ready', function(){
     }).done(function( data ) {
       $('#images').empty();
       // empties image container
-      $('h1.search-title').first()[0].innerHTML = "Search for: " + tags;
+     // $('h1.search-title').first()[0].innerHTML = "Search for: " + tags;
       $.each( data.items, function( i, item ) {
         var newListItem = $("<li>"); 
      // image information
@@ -30,7 +28,39 @@ $(document).on('ready', function(){
         var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newListItem);
         var newDescription = $('<p class="image-description">').html(item.description).appendTo(newListItem);
         var newLink = $('<a>').attr('href', item.link).text('View on Flickr.').appendTo(newListItem)
+        
+  var newButton = $("<button class='btn btn-sm btn-primary'>details</button>").attr({
+          'data-title': item.title,
+          'data-toggle': "modal",
+          'data-target': "#infoModal",
+          'data-imgsrc': item.media.m,
+          'data-description': item.description,
+          'type': "button"
+        }).appendTo(newListItem);
+        newListItem.appendTo( "#images" );
+        if ( i === 15 ) {
+          return false;
+        }
+      });
+    });
+  };
+   // Search Button 
+  $('#Search').on('click', function(event){
+
+    // Prevent the default event execution so the browser doesn't
+    event.preventDefault();
+    // Get the value of the 'input[name="searchText"]' and use that as the `tags` value you send to `searchImages()`.
+    var searchTextInput  = $("#Input").val();//$(event.target.parentElement).find('input[name="searchText"]')[0];
+    // Print search text value to console
+    console.log(searchTextInput);
+    //Execute the `searchImages()` function to fetch images for the user.
+    searchImages(searchTextInput);   alert(searchTextInput); 
+  });
+    
+ 
+ //});
+// });
+//};
  });
- });
- };
- });
+ 
+
